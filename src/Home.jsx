8f038cccfc98e26db3ddcf21e3a252c7a1300f69
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2024-01-10 03:27:00 +0800

    added new stuffs

---
+++ src/Home.jsx
@@ -0,0 +1,7 @@
+
+export const HomePage = () => {
+  return (
+    <div class="w-screen h-screen bg-slate-200">
+    </div>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import { Hono } from "hono";
+import {cors} from 'hono/cors'
 import type { FC } from "hono/jsx";
 
 const app = new Hono();
+app.use('*', cors())
 
 const Layout: FC = (props) => {
   return (
@@ -9,13 +11,23 @@ const Layout: FC = (props) => {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://unpkg.com/htmx.org@1.9.10"></script>
-        <title>Document</title>
+        <title>ByteByte API</title>
       </head>
       <body>{props.children}</body>
     </html>
   );
 };
+
+const HomePage: FC = () => {
+  return (
+    <div class={'bg-slate-200 w-full h-screen'}>
+      <h1 class={'text-2xl'}>Welcome to the BytByte API</h1>
+      <p>Please direct to the official docs and page for more info, this page is intended for developers and tools built for the site</p>
+    </div>
+  )
+}
 
 const Second: FC<{ id: string }> = (props: { id: string }) => {
   return (
@@ -34,10 +46,7 @@ app.get("/hello", (c) => {
 app.get("/", (c) => {
   return c.html(
     <Layout>
-      <h1>Hello World</h1>
-      <button hx-get="/hello" hx-swap="innerHTML">
-        Click Me
-      </button>
+      <HomePage />
     </Layout>
   );
 });
@@ -46,5 +55,14 @@ app.get("/jsx/:id", (c) => {
   const id = c.req.param("id");
   return c.html(<Second id={id} />);
 });
+
+app.get("/json/:id", (c) => {
+  const id = c.req.param('id')
+  const page = c.req.query('page')
+  return c.json({
+    idName: id,
+    pageNum: page
+  })
+})
 
 export default app;
